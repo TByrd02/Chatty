26f2ca397da78e76b2e542602f05670a2a36d984
Unknown <rbilemjian@gmail.com>
2018-10-30 19:21:37 -0700

    Chatbot process to grab info from weather API
    Using the openweathermap API to get the weather within the city inputted by the user by injecting the city into the API's URL. Then parsing the JSON received from the API and outputting the information to the chat with a concatenated string. In the case of an error, returning a JSON which lets the user know that the weather grab was unsuccessful.

--- server.js
+++ server.js
@@ -72,6 +72,30 @@ if(req.body.queryResult.action == "input.get_country"){
 
 }
 
+// get weather
+if(req.body.queryResult.action == "input.get_weather"){
+    var city = req.body.queryResult.parameters.city;
+    var url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=imperial&appid=f82c9e793551b711a8fb02587dced90f";
+    
+    var chat = "";
+    request(url, function(error, response, body){
+        var  temp = Math.round(JSON.parse(body).main.temp);
+        var weather = JSON.parse(body).weather;
+        var low =  Math.round(JSON.parse(body).main.temp_min);
+        var high = Math.round(JSON.parse(body).main.temp_max);
+        // var main = weather[0].main;
+        var description = weather[0].description;
+            var name = JSON.parse(body).name;
+            
+        if(error){
+            res.json({"fulfillmentText": "Sorry, we are having some issues grabbing the weather. Please try again."});
+        }
+                chat += "Temperature in " + name + " is " + temp + " degrees." + " With " +  description + " and a high of " + high+ " degrees " + " and a low of " + low  + " degrees. " +" Would you like to see some restaurants in "+ name +" or surrounding cities?";
+            
+            res.json({"fulfillmentText": chat});
+    });
+}
+
 
 
 });
